4cb423a5199563065eb00ae50255d09ea2a20e86
MaSoOd <42731850+masoodMEH@users.noreply.github.com>
2025-10-04 00:33:10 +0330

    bug fixed

--- src/app/pages/category/add-category/add-category.ts
+++ src/app/pages/category/add-category/add-category.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { Card } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
-import { FloatLabel } from 'primeng/floatlabel';
 import { AddCategoryRequest } from '../../../models/add-category-request.model';
 
 @Component({
   selector: 'app-add-category',
-  imports: [CommonModule, Card, ButtonModule, FormsModule, InputTextModule, FloatLabel],
+  imports: [CommonModule, Card, ButtonModule, FormsModule, InputTextModule],
   templateUrl: './add-category.html',
   styleUrl: './add-category.scss',
 })
